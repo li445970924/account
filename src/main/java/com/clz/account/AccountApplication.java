457256a7be1a4ae48da9bb210465a81920ee0b07
li445970924 <445970924@qq.com>
2019-04-17 11:16:30 +0800

    删除测试类

--- src/main/java/com/clz/account/AccountApplication.java
+++ src/main/java/com/clz/account/AccountApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AccountApplication {
 
 	public static void main(String[] args) {
-		System.out.println("yy");//test123
+		System.out.println("yy");//test12
 		SpringApplication.run(AccountApplication.class, args);
 	}
 
--- src/main/java/com/clz/account/test.java
+++
@@ -1,5 +0,0 @@
-package com.clz.account;
-
-public class test {
-    String var;
-}
